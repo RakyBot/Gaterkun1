b624ce458113482f71c245d4aae3cf466370a705
RakyBot <116264674+RakyBot@users.noreply.github.com>
2022-10-21 10:23:02 +0200

    Update rewind.ts

--- commands/Music/rewind.ts
+++ commands/Music/rewind.ts
@@ -11,11 +11,11 @@ export default class RewindCommand extends RFCommand {
 
     slashInfo = {
         name: 'rewind',
-        description: "Rewind the track currently playing.",
+        description: "🎚️ Rebobina la pista que se está reproduciendo actualmente.",
         options: [
             {
                 name: "time",
-                description: "The amount of time (seconds) to rewind the currently playing track.",
+                description: "La cantidad de tiempo (segundos) para rebobinar la pista que se está reproduciendo actualmente.",
                 type: ApplicationCommandOptionType.Integer,
                 required: true
             },
@@ -31,14 +31,14 @@ export default class RewindCommand extends RFCommand {
             const time = interaction.options.getInteger('time')
             const timePassed = Math.floor(guildQueue.currentResource.resource.playbackDuration / 1000) + guildQueue.currentResource.savedPassedTime
             
-            if (time < 1) return interaction.editReply({ embeds: [ basicEmbed( `🛑｜Please enter a time greater than 0 seconds.`, colorPalette.error ) ] })
+            if (time < 1) return interaction.editReply({ embeds: [ basicEmbed( `<:xdda:1013896171409907783> Introduzca un tiempo superior a 0 segundos.`, colorPalette.error ) ] })
 
 
             const result = await new Queue(interaction.client, queueMap).loadTrack(guild.id, guildQueue.currentTrack, timePassed - time) // Negative time because we're rewinding
             if (result) {
-                return interaction.editReply({ embeds: [ basicEmbed( `⏪｜Rewinded the track ${time} seconds.`, colorPalette.trackOperation ) ] })
+                return interaction.editReply({ embeds: [ basicEmbed( `<:xdda:1013182506461302885> Rebobinado la pista ${time} segundos.`, colorPalette.trackOperation ) ] })
             } else {
-                return interaction.editReply({ embeds: [ basicEmbed( `🛑｜Could not rewind to that time.`, colorPalette.error ) ] })
+                return interaction.editReply({ embeds: [ basicEmbed( `<:xdda:1013162318764449883> No se pudo rebobinar hasta ese momento.`, colorPalette.error ) ] })
             }
 
         })
